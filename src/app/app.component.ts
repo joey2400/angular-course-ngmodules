27cabb5ec87de3caf8ae87a06f5bc8a95f9f3263
joey2400 <joey.damedigital@gmail.com>
2025-01-10 23:47:21 +0800

    create a configurable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { UserComponent } from "./shared/user/user.component";
 import { DUMMY_USERS } from './shared/user/dummy';
+import { TasksComponent } from "./shared/tasks/tasks.component";
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId: string = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user: any) => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string) {
-    console.log('Selected user with id ' + id);
+    this.selectedUserId = id;
   }
 }
