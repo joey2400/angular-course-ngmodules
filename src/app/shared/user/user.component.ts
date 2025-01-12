6a2b08101d7ddc9fb46e80625f1a11e30d96138a
joey2400 <joey.damedigital@gmail.com>
2025-01-12 22:58:30 +0800

    Accepting Objects As Inputs & Adding Appropriate Typings

--- src/app/shared/user/user.component.ts
+++ src/app/shared/user/user.component.ts
@@ -13,9 +13,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   // Using angular decorators
-  @Input({ required: true }) id!: string;
-  @Input() avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input() avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  @Input({ required: true }) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
   @Output() select = new EventEmitter<string>();
 
   // Modern ay of using output
@@ -28,10 +34,10 @@ export class UserComponent {
 
 
   get imagePath() {
-    return 'users/' + this.avatar;
+    return 'users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
