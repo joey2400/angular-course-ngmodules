11d68a713cdf35b0daddbf7907f20268ff30445b
joey2400 <joey.damedigital@gmail.com>
2025-01-16 00:05:08 +0800

    Outputting User-specific Tasks

--- src/app/shared/tasks/tasks.component.ts
+++ src/app/shared/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, input, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { dummyTasks } from './dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -8,6 +9,12 @@ import { TaskComponent } from './task/task.component';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
   // @Input() name: string | undefined;
+  tasks = dummyTasks;
+
+  get selectedUserTasks() {
+    return this.tasks.filter(task => task.userId === this.userId);
+  }
 }
